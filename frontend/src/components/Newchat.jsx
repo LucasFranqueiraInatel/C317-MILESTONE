bf4280189c04943e58d4852594cc24822f9c14d5
Lucas Franqueira <lucas.f@gec.inatel.br>
2024-04-11 22:52:04 -0300

    :art: Adding NewChat Button

--- frontend/src/components/Newchat.jsx
+++ frontend/src/components/Newchat.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import PlusIcon from './PlusIcon'
+
 
 const Newchat = () => {
     return (
-        <div className='w-40 rounded-3xl h-10 bg-black border border-grayStroke flex justify-center items-center'>
+        <button className='w-60 rounded-[40px] h-14 pl-7 bg-black border border-grayStroke flex justify-start items-center cursor-pointer'>
+            <PlusIcon className=''/>
             <span className='font-poppins font-regular select-none'>New Chat</span>
-        </div>
+        </button>
     )
 }
 
